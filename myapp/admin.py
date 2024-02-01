18b1d4d1eeecc84dbc9ae8ff2fffc672667f5c9e
Dipec001 <dpecchukwu@gmail.com>
2024-02-01 12:22:04 +0100

    registered models in admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomUser, Comment, Like, Feed
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Comment)
+admin.site.register(Feed)
+admin.site.register(Like)
