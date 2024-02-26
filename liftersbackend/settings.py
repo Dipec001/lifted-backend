b1225628b442c2a73ee40a29dada8de906e9869d
Dipec001 <dpecchukwu@gmail.com>
2024-02-26 13:19:05 +0100

    static

--- liftersbackend/settings.py
+++ liftersbackend/settings.py
@@ -165,8 +165,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
