3f774b7e31499627825fb5b636578173f581941a
Dipec001 <dpecchukwu@gmail.com>
2024-02-26 14:05:24 +0100

    static

--- liftersbackend/settings.py
+++ liftersbackend/settings.py
@@ -165,7 +165,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 WHITENOISE_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
