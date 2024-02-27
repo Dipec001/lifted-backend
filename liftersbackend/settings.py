2873661fd888fa1d74d9d22b9d024aa97a9cbc52
Dipec001 <dpecchukwu@gmail.com>
2024-02-27 05:59:17 +0100

    media to s3 bucket

--- liftersbackend/settings.py
+++ liftersbackend/settings.py
@@ -185,22 +185,21 @@ AUTH_USER_MODEL = 'myapp.CustomUser'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 
 
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# settings.py
+# Media files (uploaded user content)
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.TokenAuthentication',
-#     ],
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ],
-# }
 
-# settings.py
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- liftersbackend/urls.py
+++ liftersbackend/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('accounts/', include('allauth.socialaccount.urls')),
 ]
 
-urlpatterns = urlpatterns + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+# urlpatterns = urlpatterns + static(
+#     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+# )
